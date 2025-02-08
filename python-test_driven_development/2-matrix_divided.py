e75a26ce0f3379f0ef059d71abb970968e8401bc
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-02-08 13:49:47 +0200

    fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """Document"""
 def matrix_divided(matrix, div):
-
-    
-    """"Document"""
+    """"Function documentation"""
     if not isinstance(matrix, (list,)):
         raise TypeError("matrix must be a matrix "
                         "(list of lists) of integers/floats")
+    
     for row in matrix:
         if type(row) != list:
             raise TypeError("matrix must be a matrix "
                             "(list of lists) of integers/floats")
+        
         for item in row:
             if not isinstance(item, (int, float)):
                 raise TypeError("matrix must be a matrix"
                                 " (list of lists) of integers/floats")
     common_size = len(matrix[0])
+    
     for row in matrix:
         if len(row) != common_size:
             raise TypeError("Each row of the matrix must have the same size")
