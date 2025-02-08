028a68fdf1b6b97c04dea522522aafe3900cc639
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-02-08 15:16:09 +0200

    dsa

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return "".join([char for char in my_string if char.lower() != 'c'])
-
+    new_str = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
+    return new_str
