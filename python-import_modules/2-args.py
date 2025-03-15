76b3250c90b5e83f5fcd94b136b8d4906978d4ee
acele-happy <a.dukundei@alustudent.com>
2025-03-15 14:41:59 -0700

    d

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1  # Exclude the script name
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
