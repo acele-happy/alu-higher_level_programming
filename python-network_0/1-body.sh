6a939e1bb44fc76ca8468f42279d4da2023babb1
acele-happy <a.dukundei@alustudent.com>
2025-03-15 15:06:44 -0700

    d

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-response=$(curl -s -w "%{http_code}" -o response.txt "$1")
-[ "$response" -eq 200 ] && cat response.txt
+# Send a GET request to the URL and display the body if the status code is 200
+[ "$(curl -s -w "%{http_code}" -o response.txt "$1")" -eq 200 ] && cat response.txt
