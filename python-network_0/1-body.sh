8eea8cd55534ca61da6f4a5bac99624a882a6309
acele-happy <a.dukundei@alustudent.com>
2025-03-15 15:04:54 -0700

    d

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Send a GET request and display the body of the response only if status code is 200
 response=$(curl -s -w "%{http_code}" -o response.txt "$1")
-if [ "$response" -eq 200 ]; then
-    cat response.txt
-fi
+[ "$response" -eq 200 ] && cat response.txt
