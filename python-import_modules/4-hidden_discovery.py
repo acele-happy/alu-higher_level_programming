25c57c858b572a63ba38d8d7c9a1a3e1368f8f1e
acele-happy <a.dukundei@alustudent.com>
2025-04-08 01:32:27 -0700

    l'

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
-import importlib.util
-import sys
-
 if __name__ == "__main__":
-    # Specify the path to the .pyc file
-    pyc_file = "hidden_4.pyc"
-    
-    # Load the compiled Python module from the .pyc file
-    spec = importlib.util.spec_from_file_location("hidden_4", pyc_file)
-    hidden_module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(hidden_module)
-
-    # Get all names defined in the module, excluding those starting with "__"
-    names = [name for name in dir(hidden_module)
+    import hidden_4
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
