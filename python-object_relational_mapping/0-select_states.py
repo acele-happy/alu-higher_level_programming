dd9b8e564c9138d87a72367b342ec778dbf41fa1
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-04-09 22:38:52 +0200

    fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-""" 0-select_states.py """
+"""
+    Program to list all states from a database hbtn_0e_0_usa
+    using sql
+"""
 
-
-import MySQLdb
 import sys
-
-
-def select_states():
-    """ lists all states from the database
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3]
-                         )
-    cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states ORDER BY id")
-
-    records = cursor.fetchall()
-    for data in records:
-        print(data)
-
-    cursor.close()
-    db.close()
+import MySQLdb
 
 
 if __name__ == "__main__":
-    select_states()
+    db_conn = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+
+    cur = db_conn.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db_conn.close()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-""" 1-filter_states.py """
+""" Program lists all states with a name
+starting with N from the database"""
 
 
-import MySQLdb
 import sys
-
-
-def select_states():
-    """ lists all states starting with 'N'
-            from the database
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3]
-                         )
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states\
-        WHERE name LIKE BINARY 'N%' ORDER BY id")
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
-
+import MySQLdb
 
 if __name__ == "__main__":
-    select_states()
+    conn = MySQLdb.connect(
+       user=sys.argv[1],
+       password=sys.argv[2],
+       db=sys.argv[3],
+       host="localhost",
+       port=3306
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states \
+        WHERE name LIKE BINARY 'N%' \
+        ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    conn.close()
