776bb87b9fdca338b897ca3d1c070cf19e1d84cb
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-06-03 09:45:44 +0200

    alu-web-scraping

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response) => {
+  if (response) {
+    console.log(`code: ${response.statusCode}`);
+  } else {
+    console.log(error);
+  }
+});
---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  } else {
+    console.log(error);
+  }
+});
